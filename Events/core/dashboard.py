af4f4483d3839ea4c16a9ecc444fbde144d525de
tanishqandmac <tanishqandmac@gmail.com>
2019-01-31 19:12:53 +0530

    dashboard v0.1

--- Events/core/dashboard.py
+++ Events/core/dashboard.py
@@ -1,8 +1,93 @@
-from .models import Users,Events
+from .models import Users,Events,Tickets,Customers
+from django.views.decorators.csrf import csrf_exempt
+from shopify_auth.decorators import login_required
+from shopify_auth.models import AbstractShopUser, ShopUserManager
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.db.models import Sum
+from django.contrib import auth
+from datetime import datetime
+from django_rq import job
+import traceback
+import requests
+import shopify
+import time
+import os
 
 def dashboard(request):
+    try:
+        Users.objects.all().delete()
+        Events.objects.all().delete()
+        Tickets.objects.all().delete()
+        Customers.objects.all().delete()
+
+        user = Users(domain_name="believer")
+        user.save()
+        user = Users.objects.get(domain_name = "believer")
+        event = Events(sno = user,
+                        event_name = "Dragons",
+                        start_date = "2019-01-28",
+                        end_date = "2019-01-31")
+        event.save()
+        event = Events.objects.filter(sno = user)
+        tickets = Tickets(sno = event[0],
+                          type = "Gold",
+                          price = 100.0)
+        tickets.save()
+        tickets = Tickets.objects.filter(sno = event[0])
+        customer = Customers(sno = tickets[0],
+                            customer_name = "tanishq",
+                            customer_email = "tanishqandmac@gmail.com")
+        customer.save()
+
+        #domain_name = str(request.user).split(".")[0]
+        domain_name = "believer"
+        userObject = Users.objects.get(domain_name = domain_name)
+        events = Events.objects.filter(sno = userObject)
+        event_details_list = []
+        for event in events:
+            event_details = {}
+            event_name = event.event_name
+            start_date = event.start_date
+            end_date = event.end_date
+            tickets = Tickets.objects.filter(sno=event)
+            tickets_sold = tickets.count()
+            revenue = sum([ticket.price for ticket in tickets])
+            event_details = {'Name':event_name,
+                             'Start_Date':start_date,
+                             'End_Date':end_date,
+                             'Tickets_Sold':tickets_sold,
+                             'Revenue':revenue}
+            event_details_list.append(event_details)
+        print (event_details_list)
+        context = {'Events':event_details_list}
+        return render(request,"core/dashboard.html",context)
+    except Exception:
+        print(traceback.format_exc())
+        return render(request,"core/error.html",{})
+
+def dashboard1(request):
     with request.user.session:
         try:
+            user = Users(domain_name="believer")
+            user.save()
+            user = Users.objects.get(domain_name = "believer")
+            event = Events(sno = user,
+                            event_name = "Dragons",
+                            start_date = "2019-01-28",
+                            end_date = "2019-01-31")
+            event.save()
+            event = Events.objects.filter(sno = user)
+            tickets = Tickets(sno = event[0],
+                              type = "Gold",
+                              price = 100.0)
+            tickets.save()
+            tickets = Tickets.objects.filter(sno = event[0])
+            customer = Customers(sno = tickets[0],
+                                customer_name = "tanishq",
+                                customer_email = "tanishqandmac@gmail.com")
+            customer.save()
+
             domain_name = str(request.user).split(".")[0]
             userObject = Users.objects.get(domain_name = domain_name)
             events = Events.objects.filter(sno = userObject)
@@ -21,6 +106,7 @@ def dashboard(request):
                                  'Tickets_Sold':tickets_sold,
                                  'Revenue':revenue}
                 event_details_list.append(event_details)
+            print (event_details_list)
             context = {'Events':event_details_list}
             return render(request,"core/dashboard.html",context)
         except:
--- Events/core/urls.py
+++ Events/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from core import views
+from core import views,dashboard
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,4 +7,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('dashboard', dashboard.dashboard, name="dashboard"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
