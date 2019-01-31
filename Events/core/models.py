3456a9d86f2b99ad97eac49976d1fd436ee3cc84
tanishqandmac <tanishqandmac@gmail.com>
2019-01-31 17:09:20 +0530

    v0.0.0 Initialisation

---
+++ Events/core/models.py
@@ -0,0 +1,5 @@
+from shopify_auth.models import AbstractShopUser
+from django.db import models
+
+class AuthAppShopUser(AbstractShopUser):
+    pass
---
+++ Events/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from core import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.index, name="index"),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
