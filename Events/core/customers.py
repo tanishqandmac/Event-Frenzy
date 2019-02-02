4d0aa046ffb328fcf4ada87026f82c122f9f670f
tanishqandmac <tanishqandmac@gmail.com>
2019-02-02 19:18:32 +0530

    data for customers list

--- Events/core/customers.py
+++ Events/core/customers.py
@@ -16,9 +16,31 @@ import os
 
 def customers(request):
     try:
+        #Data Generation
+        Users.objects.all().delete()
+        user = Users(domain_name="believer")
+        user.save()
+        for i in range(5):
+            user = Users.objects.get(domain_name="believer")
+            event = Events(sno=user,
+                            event_name = "Dragons {}".format(i),
+                            start_date = "2019-01-28",
+                            end_date = "2019-01-31")
+            event.save()
+            event = Events.objects.filter(sno = user)
+            tickets = Tickets(sno = event[i],
+                              type = "Platinum",
+                              price = 3000.0 + float(i))
+            tickets.save()
+            tickets = Tickets.objects.filter(sno = event[i])
+            customer = Customers(sno = tickets[0],
+                                customer_name = "tanishq",
+                                customer_email = "tanishqandmac@gmail.com")
+            customer.save()
+
+
         customers_list = []
         event_id = request.GET.get('event_id', '')
-
         #event = Events.objects.get(event_id = event_id)
         event = Events.objects.all()[0]
         tickets = Tickets.objects.filter(sno = event)
@@ -38,50 +60,3 @@ def customers(request):
     except Exception:
         print(traceback.format_exc())
         return render(request,"core/error.html",{})
-
-# #To be used in Production
-# def dashboard1(request):
-#     with request.user.session:
-#         try:
-#             user = Users(domain_name="believer")
-#             user.save()
-#             user = Users.objects.get(domain_name = "believer")
-#             event = Events(sno = user,
-#                             event_name = "Dragons",
-#                             start_date = "2019-01-28",
-#                             end_date = "2019-01-31")
-#             event.save()
-#             event = Events.objects.filter(sno = user)
-#             tickets = Tickets(sno = event[0],
-#                               type = "Gold",
-#                               price = 100.0)
-#             tickets.save()
-#             tickets = Tickets.objects.filter(sno = event[0])
-#             customer = Customers(sno = tickets[0],
-#                                 customer_name = "tanishq",
-#                                 customer_email = "tanishqandmac@gmail.com")
-#             customer.save()
-
-#             domain_name = str(request.user).split(".")[0]
-#             userObject = Users.objects.get(domain_name = domain_name)
-#             events = Events.objects.filter(sno = userObject)
-#             event_details_list = []
-#             for event in events:
-#                 event_details = {}
-#                 event_name = event['event_name']
-#                 start_date = event['start_date']
-#                 end_date = event['end_date']
-#                 tickets = Tickets.objects.filter(sno=event['event_id'])
-#                 tickets_sold = tickets.count()
-#                 revenue = sum([ticket['price'] for ticket in tickets])
-#                 event_details = {'Name':event_name,
-#                                  'Start_Date':start_date,
-#                                  'End_Date':end_date,
-#                                  'Tickets_Sold':tickets_sold,
-#                                  'Revenue':revenue}
-#                 event_details_list.append(event_details)
-#             print (event_details_list)
-#             context = {'Events':event_details_list}
-#             return render(request,"core/dashboard.html",context)
-#         except:
-#             return render(request,"core/error.html",{})
